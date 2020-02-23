68d5e0ea8daa3d09d065b39c8a7d7081d1124280
Daniel Gawne <danielgawne@gmail.com>
2020-02-23 12:05:01 -0500

    setup scss folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './sass/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header__text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary--main">Aporia</span>
+            <span className="heading-primary--sub">Coffee</span>
+          </h1>
+        </div>
       </header>
     </div>
   );
