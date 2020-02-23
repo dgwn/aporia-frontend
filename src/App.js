4f5347a09f7b864acf64e227232c88519de07b19
Daniel Gawne <danielgawne@gmail.com>
2020-02-23 16:26:41 -0500

    add nav button

--- src/App.js
+++ src/App.js
@@ -5,23 +5,31 @@ import './sass/main.scss';
 function App() {
   return (
     <div className="App">
+
+      <div className="navigation">
+        <input type="checkbox" className="navigation__checkbox" id="nav-toggle"/>
+        <label for="nav-toggle" className="navigation__button">
+          <span className="navigation__icon">&nbsp;</span>
+        </label>
+      </div>
+
       <header className="header">
+
         <div className="header__text-box">
           <h1 className="heading-primary">
             <span className="heading-primary--main">Aporia</span>
             <span className="heading-primary--sub">Coffee</span>
           </h1>
+        </div>
 
           <div className="home-links">
               <ul className="home-links__list">
-                <li className="home-links__item"><a href="#" className="home-links__link">Beans</a></li>
-                <li className="home-links__item"><a href="#" className="home-links__link">Our Story</a></li>
-                <li className="home-links__item"><a href="#" className="home-links__link">Contact Us</a></li>
+                <li className="home-links__item"><a href="#" className="home-links__link heading-primary--sub">Beans</a></li>
+                <li className="home-links__item"><a href="#" className="home-links__link heading-primary--sub">Our Story</a></li>
+                <li className="home-links__item"><a href="#" className="home-links__link heading-primary--sub">Contact Us</a></li>
               </ul>
           </div>
-          
 
-        </div>
       </header>
     </div>
   );
