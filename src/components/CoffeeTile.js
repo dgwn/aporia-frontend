1c456bfdef0e2bf740c8f6cb7a5065b81dacafc8
Daniel Gawne <danielgawne@gmail.com>
2020-03-06 22:14:15 -0500

    put image in coffee tile

--- src/components/CoffeeTile.js
+++ src/components/CoffeeTile.js
@@ -3,7 +3,9 @@ import React from 'react';
 const CoffeeTile = ( {bean} ) => {
     return (
         <div className="col col-1-of-4">
-            <div className="bean-card">{bean}</div>
+            <div className="bean-card">
+                <img className="bean-card__image" src={require("../img/" + bean.image )}  alt="hello"/>
+                </div>
         </div>
     )
 }
