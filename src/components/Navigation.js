cdc29138d55418f673a1bafef9f47a8ce8e659e0
Daniel Gawne <danielgawne@gmail.com>
2020-03-09 22:21:05 -0400

    add function which closes nav menu when a navigation link is clicked

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Navigation = (  ) => {
+
+    // ideally, make an onClick function that toggles the navigation__checkbox off whenever one of the navigation__links are clicked
+
+    const [navChecked, setNavChecked] = useState(false)
+    let toggleMenu = () => {
+        if (navChecked === false) {
+            setNavChecked(true)
+        }
+        else {
+            setNavChecked(false)
+        }
+
+    }
+        
+    
+
     return (
         <div className="navigation">
 
-            <input type="checkbox" className="navigation__checkbox" id="nav-toggle"/>
-            <label for="nav-toggle" className="navigation__button">
+            <input type="checkbox" className="navigation__checkbox" id="nav-toggle" checked={navChecked} onChange={toggleMenu} />
+            <label htmlFor="nav-toggle" className="navigation__button">
             <span className="navigation__icon">&nbsp;</span>
             </label>
 
@@ -13,9 +29,9 @@ const Navigation = (  ) => {
 
             <nav className="navigation__nav">
                 <ul className="navigation__list">
-                    <li className="navigation__item"><a href="#beans" className="navigation__link"><span>01</span>Beans</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link"><span>02</span>Our Story</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link"><span>03</span>Contact Us</a></li>
+                    <li className="navigation__item" ><a href="#beans" className="navigation__link" onClick={toggleMenu} ><span>01</span>Beans</a></li>
+                    <li className="navigation__item"><a href="#story" className="navigation__link" onClick={toggleMenu}><span>02</span>Our Story</a></li>
+                    <li className="navigation__item"><a href="#contact" className="navigation__link" onClick={toggleMenu}><span>03</span>Contact Us</a></li>
                 </ul>
             </nav>
         </div>
